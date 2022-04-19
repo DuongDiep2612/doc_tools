023eede4414b46b3ea624fc2af954b6aac317a04
Duong Diep <41613630+DuongDiep2612@users.noreply.github.com>
2022-04-19 12:26:22 +0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'OEE'
+copyright = '2021, ITG'
+author = 'dvdiep'
 
 release = '0.1'
 version = '0.1.0'
